cbd1642eaa5085bc7779e8fb0d75a2671f939693
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-01-15 18:38:57 +0900

    [level Lv.1] Title: 추억 점수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/Lv.1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/Lv.1/176963. 추억 점수/추억 점수.js
@@ -3,16 +3,12 @@ function solution(name, yearning, photo) {
    name.forEach((x,i) =>{
        match[x] = yearning[i]
    }) 
-    
-    let result = []
-    
-   photo.forEach((k,idx)=>{
+       
+   return photo.map((k,idx)=>{
         let sum = 0;
        k.forEach((y,i)=>{    
-
            Object.keys(match).includes(y) ? sum = sum+match[y]  : sum = sum+0 })   
-         result.push(sum)
+         return sum
    })
-    return result
     
 }
