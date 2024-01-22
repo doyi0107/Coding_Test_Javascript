d4ba6759cb473441fb09f376e2d42cf97f2558e8
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-01-22 23:56:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,23 @@
+function solution(cards1, cards2, goal) {   
+    let answer = '';
+
+   
+    for(let i=0; i< goal.length; i++) {
+     
+        if(cards1[0] == goal[i]) {
+            cards1.shift();
+            continue
+        }
+
+        else if(cards2[0] == goal[i]) {
+            cards2.shift();
+            continue
+        }
+   
+        else {
+            return "No"
+        }
+    }
+
+    return "Yes"
+}
