1ff2580cf7d853bf30d7a6da10a3b026cc636044
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-01-23 12:16:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 38.62 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,27 @@
+function solution(participant, completion) {
+    let answer;
+     let hashmap = new Map();
+     
+     participant.forEach((x,i)=>{
+         if(hashmap.has(x)){
+             hashmap.set(x, hashmap.get(x)+1)
+             
+         }else{
+             hashmap.set( x, 1)
+         }
+     })
+    
+    completion.forEach((x,i) => {
+         if(hashmap.has(x)){
+             hashmap.set( x, hashmap.get(x)-1)
+         }
+    })
+    
+    for(let [key,value] of hashmap){
+        if(value === 1){
+            answer = key
+        }
+    }
+    
+    return answer
+}
