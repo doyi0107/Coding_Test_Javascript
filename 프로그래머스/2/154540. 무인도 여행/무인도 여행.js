e2df6c6c1d169387f2c69b1115928646ace14c89
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-02-04 00:25:51 +0900

    [level 2] Title: 무인도 여행, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,37 +1,39 @@
 function solution(maps) {
-    const arrMap = maps.map((string) => string.split(""));
-    const rowLength = arrMap.length - 1;
-    const columnLength = arrMap[0].length - 1;
-    const dRow = [1, 0, -1, 0];
-    const dCol = [0, 1, 0, -1];
-    const result = [];
-
-    const bfs = (row, col, count) => {
-        if (row > rowLength || col > columnLength || row < 0 || col < 0) {
-            return count;
+    let newMaps = maps.map((x) => x.split(''))
+    let result = [];
+    let cols = maps[0].length;
+    let rows = maps.length; 
+    let direct = [[0,1],[1,0],[0,-1],[-1,0]]
+    
+    const dfs = (rowIdx,colIdx,count) => {
+        if(rowIdx < 0 || colIdx < 0 || rowIdx >= rows || colIdx >= cols){
+            return count
+         }
+        
+        if(newMaps[rowIdx][colIdx] === "X"){
+            return count
         }
-        if (arrMap[row][col] === "X") {
-            return count;
+        
+        count += Number(newMaps[rowIdx][colIdx])
+        newMaps[rowIdx][colIdx] = 'X'
+        
+        for(let i = 0; i<4; i++){
+            count = dfs(rowIdx + direct[i][0], colIdx + direct[i][1],count)
         }
-
-        count += Number(arrMap[row][col]);
-        arrMap[row][col] = "X";
-
-        for (let i = 0; i < 4; i++) {
-            count = bfs(row + dRow[i], col + dCol[i], count);
-        }
-
-        return count;
-    };
-
-    for (let i = 0; i <= rowLength; i++) {
-        for (let j = 0; j <= columnLength; j++) {
-            if (arrMap[i][j] === "X") {
-                continue;
-            }
-            result.push(bfs(i, j, 0));
-        }
+        
+        return count
     }
-
-    return result.length ? result.sort((a, b) => a - b) : [-1];
+    
+    
+    
+     for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols ; j++) {
+                if (newMaps[i][j] === "X") {
+                    continue;
+                }
+                result.push(dfs(i, j, 0));
+            }
+        }
+        
+    return result.length ? result.sort((a,b) => a-b) : [-1]
 }
