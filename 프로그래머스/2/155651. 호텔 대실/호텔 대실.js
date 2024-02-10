138599010f8d1d9d1e3083afad9bae6718706dcc
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-02-10 22:07:03 +0900

    [level 2] Title: 호텔 대실, Time: 5.34 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,22 +1,20 @@
-function solution(book_time) {
-     let result = []
-   let array = book_time.map(([start,end])=> {
-           let [start_hour,start_minute] = start.split(":").map((x) => Number(x));
-         let [end_hour,end_minute] = end.split(":").map((x) => Number(x));
-        return [start_hour*60 + start_minute , end_hour*60+end_minute+10]
-   })
-   
-   array.sort((a,b) => a[0] - b[0])
-    
-   array.forEach(([start,end]) => {
-       if(Math.min(...result) > start){
-           result.push(end)
-         }else{
-             const minidx = result.indexOf(Math.min(...result))
-             result.splice(minidx,1,end)
-            }
-   })
-    
-    return result.length
+function makeMinStamp(time) {
+    const [hour, min] = time.split(":").map(v => Number(v));
+    return hour * 60 + min;
+}
 
+function solution(book_time) {
+    const timeArr = Array.from({ length: makeMinStamp('23:59') + 10 }, () => 0);
+
+    book_time.forEach((time, i) => {
+        const [s, e] = time;
+        let start = makeMinStamp(s);
+        const end = makeMinStamp(e) + 9;
+
+        for (start; start <= end; start++) {
+            timeArr[start]++;
+        }
+    });
+
+    return Math.max(...timeArr);
 }
