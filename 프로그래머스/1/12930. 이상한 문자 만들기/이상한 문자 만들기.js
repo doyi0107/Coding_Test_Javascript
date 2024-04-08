69233725a216990260dbfee26790136d77ca594d
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-04-08 12:40:44 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,8 +1,20 @@
 function solution(s) {
-     let result = [];
-    for(let i=0; i< s.length; i++){
-       i%2 === 1 ? result.push(s[i].toLowerCase()) : result.push(s[i].toUpperCase())
+    let result = '';
+    let isEven = true;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            result += ' ';
+            isEven = true; // 공백을 만나면 다음 문자는 짝수번째 문자로 시작하게 됨
+        } else {
+            if (isEven) {
+                result += s[i].toUpperCase();
+            } else {
+                result += s[i].toLowerCase();
+            }
+            isEven = !isEven;
+        }
     }
-    console.log(result)
-    return result.join('')
+
+    return result;
 }
