2c144067b0d98541982f2328aad50b9ec352d5af
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-05-25 14:19:05 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,8 @@
 function solution(num) {
      let count = 0;
      while(num != 1){
-         if(num%2 == 0){
-             num = num/2;
-             count++;
-         }else{
-            num = num*3 + 1;
-            count++;
-         }
+        num%2 == 0 ? num = num/2 :   num = num*3 + 1;
+        count++;
      }
     
     return count>500 ? -1 : count
