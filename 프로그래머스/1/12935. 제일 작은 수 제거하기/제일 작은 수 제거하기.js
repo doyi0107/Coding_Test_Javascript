aee779c00e36e7f40f7b6143a798662fc08a0a1d
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-06-04 22:06:04 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,4 @@
 function solution(arr) {
-    let sortedarr = [...arr].sort((a,b) => (a-b))
-    let result = arr.filter((x) => x !== sortedarr[0])
-    //
+    let result = arr.filter((x) => x !== Math.min(...arr))
     return  arr.length > 1 ? result : [-1]
 }
