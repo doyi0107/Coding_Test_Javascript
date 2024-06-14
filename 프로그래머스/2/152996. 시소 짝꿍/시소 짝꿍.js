2df7bcd9524960cb04ccbd07b64c23e753350abf
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-06-14 17:16:29 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,38 +1,14 @@
 function solution(weights) {
-    let answer = 0;
+  weights.sort((a, b) => b - a);
+  const dict = {};
+  return weights.reduce((cnt, w) => {
+    if (dict[w]) cnt += dict[w]; // 1:1
 
-    // 카운트 배열 생성
-    const cntWeight = new Array(1001).fill(0);  // 몸무게 원본 저장
-    const cntMul = new Array(4001).fill(0);     // 몸무게 배수 저장
-
-    // weights 배열 순회
-    for (let i = 0; i < weights.length; i++) {
-        // 현재 사람의 몸무게 체크
-        const weight = weights[i];
-        const temp = cntWeight[weight];
-        const m2 = weight * 2;
-        const m3 = weight * 3;
-        const m4 = weight * 4;
-
-        if (temp > 0) { // 앞에서 같은 몸무게가 있었다면
-            // 그 사람 수 만큼 카운트 ++
-            answer += temp;
-            // 몸무게 2, 3, 4배 체크하기
-            answer += cntMul[m2] - temp;
-            answer += cntMul[m3] - temp;
-            answer += cntMul[m4] - temp;
-        } else { // 앞에서 같은 몸무게가 없었다면
-            answer += cntMul[m2];
-            answer += cntMul[m3];
-            answer += cntMul[m4];
-        }
-
-        // 배열에 카운트 더하기
-        cntWeight[weight]++;
-        cntMul[m2]++;
-        cntMul[m3]++;
-        cntMul[m4]++;
-    }
-
-    return answer;
+    if (dict[w * 4/3]) cnt += dict[w * 4/3]; // 3:4
+    if (dict[w * 3/2]) cnt += dict[w * 3/2]; // 2:3
+    if (dict[w * 2]) cnt += dict[w * 2]; // 2:4
+    // 사전 갱신해주기
+    dict[w] = (dict[w] || 0) + 1;
+    return cnt;
+  }, 0);
 }
