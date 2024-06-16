d1253f7c90ed4b9420273d3440699dd41d5ea22f
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-06-16 12:28:45 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,35 +1,16 @@
 function solution(progresses, speeds) {
-    let arr = [],result = [];
-    
-    progresses.forEach( (x,i) => {
-        let count = 0; 
-          while(x < 100){
-             x += speeds[i]
-              count += 1
-          }
-         arr.push(count)
-     })
-    
-
+   let result = [0];
+    let days = progresses.map((x,i) => { return Math.ceil((100-x)/speeds[i])})
+   let Maxdays = days[0]
    
- for (let i = 0; i < arr.length; i++) {
-
-        let count = 1; // 자기 자신도 포함하므로 1로 시작
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[i] >= arr[j]) {
-                count += 1;
-            } else {
-                break;
-            }
-        }
-
-        // 3)
-       result.push(count);
-
-        // 4)
-        i += count - 1; // 이미 계산한 작업은 건너뛰기
-    }
+   for(let i = 0, j=0; i<days.length; i++){
+       if(Maxdays >= days[i]){
+           result[j] += 1
+       }else{
+           Maxdays = days[i]
+           result[++j] = 1;
+      }
+   }
     
-       return result
- 
+    return result
 }
