508604b39d7cecfa8b1ed949ee8751103745cd4e
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-06-17 22:16:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 12.38 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr){
+    let stack = [];
+       stack.push(arr[0])
+    for(let i = 0; i<arr.length; i++){
+        if(stack[stack.length-1] !== arr[i]){
+            stack.push(arr[i])
+        }
+    }
+  return stack
+}
