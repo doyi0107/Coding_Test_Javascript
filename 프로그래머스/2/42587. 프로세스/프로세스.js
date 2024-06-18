19912e77a342dcd3d34f17d9638ed92aa6f29aaf
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-06-18 13:54:01 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,20 +1,18 @@
 function solution(priorities, location) {
-    let answer = 0;
-    let twoarr = priorities.map((x,i) =>  [x,i])
-    let arr = []
-    
-    console.log(twoarr)
-    for(let i=0; i<priorities.length; i++){
-        let temp = twoarr.shift();
-        console.log( temp)
-        if(twoarr.some((x)=> temp[0] < x[0])){
-             twoarr.push(temp)
-        }else{
-            arr.push(temp)
-            if(arr[answer][1] == location) break;
-            else answer ++;
-        }
-    }
-    
-    return answer+1;
+     let objpri = priorities.map((x,i) => [x,i])
+     let arr = [];
+     let result = 0;
+     
+     
+     for(let i = 0; i<objpri.length;){
+          let temp = objpri.shift();
+         if(objpri.some((x) => temp[0] < x[0])){ objpri.push(temp)}
+ 
+         else{
+             arr.push(temp)
+             if(arr[result][1] === location)break;
+             else{result++;}
+         }
+     }
+     return result+1
 }
