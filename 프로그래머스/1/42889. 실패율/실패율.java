eedba8541d377ca60d17be448e66e0de9aaecc76
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-09-22 03:05:36 +0900

    [level 1] Title: 실패율, Time: 5.59 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,17 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
+          // 실패율과 인덱스값 매치
           double[][] result = new double[N][2];
+          // 분자값
           int[] arr = new int[N];
+          // 분모값 계산을 위한 sum
           int sum = 0;
+          // 정답
           int[] answer = new int[N];
         
+         // 분자값
          for(int i=0; i<stages.length; i++){
              if(stages[i] != N+1){
                 arr[stages[i]-1]++; 
               }
           }
         
+        // 실패율 계산
          for(int i=0; i<result.length; i++){
              if(stages.length-sum > 0){
                result[i][0] = (double)arr[i]/(stages.length-sum);        
@@ -24,6 +30,7 @@ class Solution {
         }
 
         
+        // 람다식을 이용할 내림차순 정렬을 위해 Double[][]로 교체 
         Double[][] boxedResult = new Double[N][2];
         for (int i = 0; i < N; i++) {
             boxedResult[i][0] = result[i][0];
@@ -32,6 +39,7 @@ class Solution {
 
         Arrays.sort(boxedResult, (a,b) -> Double.compare(b[0],a[0]));
     
+        // 정답을 출력하기 위해 2차원 배열에서 인데스값만 추출
         for(int i=0; i<N; i++){
            answer[i] = boxedResult[i][1].intValue();
         }
