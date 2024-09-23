ef424b28e9009206680a6399083eb6fec1801385
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-09-23 23:50:26 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int n = Integer.parseInt(br.readLine());
+       String[] arr = br.readLine().split(" ");
+       int[] IntArr = new int[n];
+
+       //String배열 int배열로 변환하기
+       for(int i=0; i<n; i++){
+           IntArr[i] = Integer.parseInt(arr[i]);
+       }
+
+       Arrays.sort(IntArr);
+       int sum = 0;
+       int[] sumArr = new int[n];
+
+       // 각 사람마다 걸리는 시간 배열에 집어 넣기
+       for(int i=0; i<n; i++){
+           sum+= IntArr[i];
+           sumArr[i] = sum;
+       }
+
+       // 마지막 총합 구하기
+       int result = 0;
+       for(int i=0; i<n; i++){
+          result+=sumArr[i];
+       }
+
+    System.out.println(result);
+
+  }
+}
