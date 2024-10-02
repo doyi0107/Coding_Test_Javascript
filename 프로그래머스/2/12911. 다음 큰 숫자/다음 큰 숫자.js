c838dd60bcdeb14d7f83421ff8f96bbc9934458f
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-10-02 22:14:13 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.22 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,7 +1,11 @@
 function solution(n) {
+    // n보다 큰수
     let answer = n+1;
+    
+    // n보다 큰 수 중에 1의 개수 같은 거 찾기 
     while(true){
         if(countOne(n) === countOne(answer))return answer;
+        // 1의 개수 틀리면 answer증가
         else answer++;
     }
 }
@@ -10,8 +14,10 @@ const countOne = (n) => {
     let numOfOne = 0;
     let binary = [];
     
+    //n 이진수로 만들기 
     binary = n.toString(2)
     
+    // binary에서 1의 개수 
     for(let char of binary) if(char === '1')numOfOne++;
     return numOfOne;
 
