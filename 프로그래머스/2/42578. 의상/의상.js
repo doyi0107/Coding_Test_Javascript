f6d7e9060d550f08a83152676a12473fb6971103
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-10-09 01:31:50 +0900

    [level 2] Title: 의상, Time: 0.29 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,24 +1,11 @@
 function solution(clothes) {
-    let object = {}, cnt = 1 , answer = 1;
+    let object = {}, cnt = 1
     
     // 똑같은 옷 종류의 개수, 객체로 표현해주기
     for(let i=0; i<clothes.length; i++){  
         const type = clothes[i][1]
-        if(object[type] != undefined){
-            object[type] += 1;
-        }else{
-            object[type] = cnt;
-         }
+        object[type] != undefined ?  object[type] += 1 : object[type] = cnt
     }
-
-    // 옷의 종류가 하나 이상일 때 조합공식 활용하기 
-   if(Object.keys(object).length > 1){
-       for(let key in object){   
-           let value = object[key]
-           answer *= (value+1)         
-       }
-   }
-
-    
-    return Object.keys(object).length > 1 ? answer-1 : clothes.length
+   
+    return Object.values(object).reduce((acc,x) => acc*(x+1),1)-1
 }
