c1c2a547df54453e96b3f1e574570715284775ca
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-10-09 23:39:29 +0900

    [level 1] Title: 덧칠하기, Time: 2.69 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,6 +1,10 @@
 function solution(n, m, section) {
    let result = 0;
+    
+    // 초기값으로 음의 무한대값으로 설정 
    let maxRange = -Infinity;
+    
+    //section값 돌면서 원소가 maxRange보다 값이 작으면 result +1 = 페인트를 다시 칠해야하는 범위
     section.forEach(x => {
         if(maxRange < x){
            result++;
