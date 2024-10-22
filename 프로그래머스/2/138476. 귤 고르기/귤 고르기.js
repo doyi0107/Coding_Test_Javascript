2104b89c9a0ec2f4d7b42a943360cc4bfc82a935
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-10-22 23:48:56 +0900

    [level 2] Title: 귤 고르기, Time: 136.08 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,17 +1,18 @@
 function solution(k, tangerine) {
-    let count = 0, result = 0, object = {};
+    let count = 0,result = 0, object = {}; 
+
+  // 숫자와 배열 안의 숫자 개수 매치하기
+   tangerine.forEach((x,idx) => {
+       object[x] === undefined ? object[x] = 1: object[x]++  
+   })      
     
-    tangerine.forEach((x,i) => {
-        object[x] == undefined ? object[x] = 1 : object[x]++
+    //여기서 2차원 배열로 바뀜
+   let gulnum = Object.entries(object).sort( ([,a] , [,b]) => b - a);
+   
+    gulnum.forEach ((i,index) => {  
+           if(count < k)
+           { count += Number(gulnum[index][1]);  result++; }       
     })
-    
-    let gulnum = Object.entries(object).sort(([,a],[,b]) => b-a)
-    
-    gulnum.forEach((x,i) => {
-        if( count < k){
-            count += x[1]; result++
-        }
-    })
-    
-    return result
+
+   return result
 }
