4194640cb6e4a24ab908cc739a387d81e551b6e1
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-10-24 23:09:19 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+     if(n == 1 || n == 2) return n;
+    
+     let dp = [1,2];
+    
+     for(let i=2; i<n; i++){
+         dp[i] = (dp[i-1] + dp[i-2])%1234567
+     }
+    
+     return dp[n-1]
+}
