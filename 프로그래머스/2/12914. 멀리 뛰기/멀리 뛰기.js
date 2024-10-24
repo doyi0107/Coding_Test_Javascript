90711f6eb943b2080db62b419eb9464381e407a2
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-10-24 23:17:28 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,11 @@
 function solution(n) {
      if(n == 1 || n == 2) return n;
     
+    //초기값설정
      let dp = [1,2];
     
      for(let i=2; i<n; i++){
+         //피보나치 점화식 적용
          dp[i] = (dp[i-1] + dp[i-2])%1234567
      }
     
