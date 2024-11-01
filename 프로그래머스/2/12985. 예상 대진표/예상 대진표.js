6a25830c151b84e645aeed2676270ca618cbc74d
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-11-02 00:29:15 +0900

    [level 2] Title: 예상 대진표, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -2,10 +2,15 @@ function solution(n,a,b){
     let answer = 1;
 
     while(a !== b){
+        
+        // 다음 대진표에서의 순서
          a = Math.round(a/2);
          b = Math.round(b/2);
          answer++; 
+        
+        // 순서가 같아지는 순간 나옴 
         if(a == b){
+            // 순서가 같을 때가 아니라 서로 붙게되는 라운드를 구하는 거기 때문에 -1
              answer--;
          }
     }
