7ce8adc5b1c6fc68d85eb5587e7750237e4984e8
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-11-04 22:23:12 +0900

    [level 2] Title: 구명보트, Time: 57.04 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,19 @@
+function solution(people, limit) {
+    let result = 0; 
+    people.sort((a,b) => a-b);
+    let stack = []
+    
+    while(people.length > 0){
+        stack.push(people.pop());
+        let light = people[0];
+        if(stack[stack.length-1] + light <= limit){
+            result++;
+            people.shift();
+        }else{
+            result++;
+        }
+        
+    }
+    
+    return result
+}
