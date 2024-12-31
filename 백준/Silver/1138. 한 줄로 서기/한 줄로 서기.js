f3b0c3bb48cab6d0aa06e625e027714d102803f5
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2024-12-31 21:55:15 +0900

    [Silver II] Title: 한 줄로 서기, Time: 100 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let n = input[0]
+let arr = input[1].split(' ').map(Number);
+let order = Array.from({length:n}, (_,i) => i+1)
+let result = [];
+
+for(let i = n-1; i>=0; i--){    
+    result.splice(arr[i],0,order[i])
+}
+
+console.log(result.join(' '))
