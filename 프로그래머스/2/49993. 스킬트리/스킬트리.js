da5abf6253f0f8e53d8c0903a47d7adbfd096a99
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-01-19 15:40:01 +0900

    [level 2] Title: 스킬트리, Time: 7.49 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -0,0 +1,10 @@
+function solution(skill, skill_trees) {
+    let answer = 0;
+    let object = {};
+    skill.split("").forEach((x) => object[x] =1);
+    console.log(object)
+    
+    return skill_trees.map((x) => x.split("").filter(y => Boolean(object[y])).join(""))
+    .filter((x) => Boolean(skill.startsWith(x))).length;
+
+}
