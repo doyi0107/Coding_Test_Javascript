0fb538b2a85555187677dda1da9c7f1b42e7a55a
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-01-25 19:33:15 +0900

    [Silver I] Title: 행렬, Time: 156 ms, Memory: 10168 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.js
@@ -0,0 +1,54 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [N,M] = input[0].split(' ').map(Number)
+
+let A = [];
+let B = [];
+for(let i=1; i<N+1; i++){ 
+    let arr = input[i].trim().split("").map(Number);  
+    A.push(arr);
+}
+
+for (let i = N+1; i < input.length; i++) {
+  let arr = input[i].trim().split("").map(Number);  
+  B.push(arr);
+}
+
+let count = 0;
+
+const flip3X3 = (matrix, k,m) => {
+  for(let i=0; i<3; i++){
+    for(let j=0; j<3; j++){
+        matrix[i + k][j + m] == 0
+          ? (matrix[i + k][j + m] = 1)
+          : (matrix[i+k][j+m] = 0);
+    }
+  }
+}
+
+for(let i=0; i<=N-3; i++){
+  for(let j=0; j<=M-3; j++){
+    if(A[i][j] !== B[i][j]){
+       flip3X3(A,i,j);
+       count++;
+    }
+  }
+}
+
+const isSame = (a,b) => {
+    for(let i=0; i<a.length; i++){
+        for(let j=0; j<a[0].length; j++){
+            if(a[i][j] !== b[i][j]){
+              return false;
+            }
+        }
+    }
+
+
+    return true;
+}
+
+
+console.log(isSame(A,B) ? count : -1 )
+
+
