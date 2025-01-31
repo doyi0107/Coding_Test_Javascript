e3d437a76dfbb974acb445b71dad0f2200a878bc
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-01-31 18:26:29 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,29 +1,32 @@
 function solution(n, computers) {
-    let visited = Array(n).fill(false); // 방문 여부 체크 배열
-    let result = 0; // 네트워크 개수
+    let result = 0; 
+    let visited = Array(n).fill(false);
     
     const bfs = (start) => {
         let queue = [start];
         visited[start] = true;
         
-        while (queue.length) {
+        while(queue.length){
             let node = queue.shift();
             
-            for (let i = 0; i < n; i++) {
-                if (computers[node][i] === 1 && !visited[i]) {
-                    visited[i] = true;
-                    queue.push(i);
+            for(let i=0; i<n; i++){
+            if(computers[node][i] == 1 && computers[i][node] ==1 && !visited[i]){
+                visited[i] = true;
+                queue.push(i)
                 }
             }
         }
-    };
-    
-    for (let i = 0; i < n; i++) {
-        if (!visited[i]) {
-            bfs(i);
-            result++; // 새로운 네트워크 발견 시 카운트 증가
-        }
     }
     
+    for(let i=0; i<n; i++){
+        if(!visited[i]){
+            bfs(i)
+            result++;
+        }
+    }
+   
+    
+  
     return result;
+    
 }
