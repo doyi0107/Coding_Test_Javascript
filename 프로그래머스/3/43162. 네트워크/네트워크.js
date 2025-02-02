160ec55f717071db7d56d8b82151cb08130d0515
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-02 12:09:53 +0900

    [level 3] Title: 네트워크, Time: 0.82 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,32 +1,28 @@
+let visited;
+let arr;
+
 function solution(n, computers) {
-    let result = 0; 
-    let visited = Array(n).fill(false);
-    
-    const bfs = (start) => {
-        let queue = [start];
-        visited[start] = true;
-        
-        while(queue.length){
-            let node = queue.shift();
-            
-            for(let i=0; i<n; i++){
-            if(computers[node][i] == 1 && computers[i][node] ==1 && !visited[i]){
-                visited[i] = true;
-                queue.push(i)
-                }
-            }
-        }
-    }
-    
-    for(let i=0; i<n; i++){
-        if(!visited[i]){
-            bfs(i)
-            result++;
-        }
-    }
+    visited = Array(n).fill(false);
+    arr = computers;
+    let result = 0;
    
+    for(let i=0; i<n; i++){
+        result += dfs(i);
+    }
     
-  
     return result;
+}
+
+const dfs = (start) => {
+    if(visited[start]) return 0;
+    else visited[start] = true;
+    
+    for(let j in arr[start]){
+        if(arr[start][j] == 1){
+            dfs(j)
+        }
+    }
+        
+    return 1;
     
 }
