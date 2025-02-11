cdd6e8bf543efb4777ee09ae8c535eb291015179
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-11 13:55:22 +0900

    [level 3] Title: 베스트앨범, Time: 4.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,40 +1,22 @@
 function solution(genres, plays) {
-    let map = []
-    for(let i=0; i<plays.length; i++){
-        map.push([plays[i], i]);
-    }
-    
-    let mapArr = map.sort((a,b) => {
-        if(a[0] === b[0]){
-            return a[1] - b[1]
-        }
-        return b[0] - a[0]
+    var dic = {};
+    genres.forEach((t,i)=> {
+        dic[t] = dic[t] ?  dic[t] + plays[i] :plays[i];        
     });
-    console.log(mapArr)
-    let genreSum = new Map();
-    for(let i=0; i<genres.length; i++){
-        if(genreSum.has(genres[i])){
-              genreSum.set(genres[i],  genreSum.get(genres[i]) + plays[i])
-        }else{
-            genreSum.set(genres[i], plays[i])
-        }
-    }
     
-    let genreSumArr = [...genreSum].sort((a,b) => b[1] - a[1]);
-    let result = [];
-    let count = 0;
-    for(let i=0; i<genreSumArr.length; i++){
-          for(let j = 0; j<mapArr.length; j++){         
-                 if(genreSumArr[i][0] == genres[mapArr[j][1]]){
-                     count++;
-                     result.push(mapArr[j][1])
-                 }
-                if(count == 2) break;
-          }
-          count = 0; 
-    }
-    
-    
-    return result
-
+    console.log(dic)
+    var dupDic = {};
+    return genres          
+          .map((t,i)=> ({genre : t, count:plays[i] , index:i}))
+          .sort((a,b)=>{               
+               if(a.genre !== b.genre) return dic[b.genre] - dic[a.genre];
+               if(a.count !== b.count) return b.count - a.count;
+               return a.index - b.index;
+           })
+           .filter(t=>  {
+               if(dupDic[t.genre] >= 2) return false;
+               dupDic[t.genre] = dupDic[t.genre] ? dupDic[t.genre]+ 1 : 1;
+               return true;
+            })
+           .map(t=> t.index);    
 }
