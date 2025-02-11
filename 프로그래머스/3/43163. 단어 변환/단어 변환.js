729f20b483e06ba0878e603c5b822e44b4da7e67
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-11 23:07:59 +0900

    [level 3] Title: 단어 변환, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,35 +1,37 @@
 function solution(begin, target, words) {
-    let result = 0;
-    let visited = Array(words.length).fill(false);
-
-    if (!words.includes(target)) return 0;
-
-    const changeWord = (word, curWord) => {
-        let count = 0;
-        let index;
-        for (let i = 0; i < word.length; i++) {
-            if (curWord[i] !== word[i]) {
-                index = i;
-                count++;
-            }
-        }
-        return count === 1 ? index : -1;
-    };
-
-    let queue = [[begin, 0]]; // 현재 단어와 변환 횟수를 함께 저장
-
-    while (queue.length > 0) {
-        let [curWord, steps] = queue.shift();
-
-        if (curWord === target) return steps; // 목표 단어 도달 시 변환 횟수 반환
-
-        for (let i = 0; i < words.length; i++) {
-            if (!visited[i] && changeWord(words[i], curWord) !== -1) {
-                visited[i] = true;
-                queue.push([words[i], steps + 1]);
-            }
-        }
+    if(!words.includes(target)) return 0
+    let visited = Array(words.length).fill(false)
+    
+    const changeWord = (word,node) => {
+          let index;
+          let count = 0; 
+          for(let i=0; i<word.length; i++){
+              if(node[i] !== word[i]){
+                  index = i; 
+                  count++;
+              }
+          }
+        
+          if(count == 1) return count
+        
+         return -1
     }
-
-    return 0;
+    
+    
+     let queue = [[begin,0]]
+     
+     while(queue.length){
+         let [node,step] = queue.shift();
+         
+         if(node == target) return step
+         
+         for(let i=0; i<words.length; i++){
+              if(!visited[i] && changeWord(words[i],node) !== -1){
+                  visited[i] = true;
+                  queue.push([words[i], step+1])
+              }
+        }
+     }
+    
+    return 0
 }
