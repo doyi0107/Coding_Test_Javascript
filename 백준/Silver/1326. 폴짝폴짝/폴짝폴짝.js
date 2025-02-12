379ba75443c39c297782dda65f0e82edebd3c596
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-12 14:53:29 +0900

    [Silver II] Title: 폴짝폴짝, Time: 896 ms, Memory: 29112 KB -BaekjoonHub

---
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.js
@@ -0,0 +1,31 @@
+let input = require('fs').readFileSync("/dev/stdin").toString().split("\n");
+
+let N = Number(input[0]);
+let bridge = input[1].split(" ").map(Number);
+let [a, b] = input[2].split(" ").map(Number);
+
+let visited = Array(N).fill(false);
+let queue = [[a - 1, 0]];
+
+while (queue.length) {
+    let [index, step] = queue.shift();
+
+    if (index + 1 === b) return console.log(step);
+
+    if (!visited[index]) {
+        visited[index] = true;
+        let jump = bridge[index];
+
+        // 왼쪽으로 점프
+        for (let i = index - jump; i >= 0; i -= jump) {
+            if (!visited[i]) queue.push([i, step + 1]);
+        }
+
+        // 오른쪽으로 점프
+        for (let i = index + jump; i < N; i += jump) {
+            if (!visited[i]) queue.push([i, step + 1]);
+        }
+    }
+}
+
+console.log(-1);
