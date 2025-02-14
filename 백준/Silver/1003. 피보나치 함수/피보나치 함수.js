7008647e1d52a9808ca4dc6a3dd65379124bdb57
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-02-14 22:09:29 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 9752 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,20 +1,19 @@
-let input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
+//x
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n").map(Number);
 
-let dp = Array.from({ length: 41 }, () => [0,0]);
+let T = input[0];
 
-dp[0] = [1,0];
-dp[1] = [0,1];
+let dp = Array.from({length:41}, () => [0,0]);
 
-for (let i = 2; i <= 40; i++) {
-  dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
-  dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+dp[0][0]++;
+dp[1][1]++;
+
+
+for(let i=2; i<=40; i++){
+   dp[i][0] = dp[i-1][0] + dp[i-2][0];
+   dp[i][1] = dp[i-1][1] + dp[i-2][1];
 }
 
-for (let i = 1; i < input.length; i++) {
-  let x = Number(input[i]);
-  console.log(dp[x].join(" "));
+for(let i=1; i<=T; i++){
+   console.log(dp[input[i]].join(" "))
 }
