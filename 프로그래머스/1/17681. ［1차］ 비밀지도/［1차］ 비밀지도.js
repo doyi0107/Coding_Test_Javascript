50c628499b0886ad9c9e222dbfdf3bc9eb6540fd
LEEDOYI <93458143+doyi0107@users.noreply.github.com>
2025-05-07 22:39:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -2,10 +2,9 @@ function solution(n, arr1, arr2) {
     let answer = [];
 
     for (let i = 0; i < arr1.length; i++) {
-        const binaryNumber = (arr1[i] | arr2[i]).toString(2);
-        const setDigit = String(binaryNumber).padStart(n, "0");
-        const str = setDigit.replace(/[1-9]/g, "#").replace(/[0]/g, " ");
-        answer.push(str);
+        let binaryNumber = (arr1[i] | arr2[i]).toString(2);
+        let row = ' '.repeat(n-binaryNumber.length).concat(binaryNumber).replace(/1/gi,'#').replace(/0/gi,' ');
+        answer.push(row)
     }
     return answer;
 }
